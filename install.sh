d6eae87fcc7549ef399a0a710caa7c7a2ee39bed
Youchen Lee <copyleft@Youchenteki-MacBook-Air.local>
2015-04-29 15:44:48 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 cp git-completion.bash ~/.git-completion.bash
 
+
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+cp git-prompt.sh ~/.git-prompt.sh
+
