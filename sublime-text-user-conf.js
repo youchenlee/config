3929fa427f9a1660ddae2382a2e7df64b1de16c6
Youchen Lee <copyleft@utcr.org>
2017-07-15 16:22:58 +0800

    add sublime text conf (thanks @storyn26383)

---
+++ sublime-text-user-conf.js
@@ -0,0 +1,69 @@
+{
+    "bold_folder_labels": false,
+    "default_font_size": 18,
+    "draw_centered": false,
+    "file_exclude_patterns":
+    [
+        "*.pyc",
+        "*.pyo",
+        "*.exe",
+        "*.dll",
+        "*.obj",
+        "*.o",
+        "*.a",
+        "*.lib",
+        "*.so",
+        "*.dylib",
+        "*.ncb",
+        "*.sdf",
+        "*.suo",
+        "*.pdb",
+        "*.idb",
+        ".DS_Store",
+        "*.class",
+        "*.psd",
+        "*.db",
+        "*.sublime-workspace",
+        ".tags",
+        ".tags_sorted_by_file",
+        ".DS_Store"
+    ],
+    "folder_exclude_patterns":
+    [
+        ".svn",
+        ".git",
+        ".hg",
+        "CVS",
+        "bower_components",
+        "node_modules"
+    ],
+    "font_face": "Source Code Pro",
+    "font_options":
+    [
+        "gray_antialias"
+    ],
+    "font_size": 18,
+    "highlight_line": true,
+    "highlight_modified_tabs": true,
+    "ignored_packages":
+    [
+    ],
+    "indent_guide_options":
+    [
+        "draw_normal",
+        "draw_active"
+    ],
+    "line_padding_bottom": 5,
+    "line_padding_top": 5,
+    "scroll_past_end": false,
+    "sidebar_row_padding_medium": true,
+    "sidebar_size_12": true,
+    "tabs_small": true,
+    "translate_tabs_to_spaces": true,
+    "trim_trailing_white_space_on_save": true,
+    "update_check": false,
+    "vintage_ctrl_keys": true,
+    "vintage_start_in_command_mode": true,
+    "ignore_packages": ["vintage"],
+    "word_wrap": true
+}
