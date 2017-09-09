867c5a15d923a9bbd140a6dee9b46980e9e18c21
Youchen Lee <copyleft@utcr.org>
2017-09-09 22:22:57 +0800

    update dotfiles

--- .emacs.d/lisp/init-local.el
+++ .emacs.d/lisp/init-local.el
@@ -58,6 +58,10 @@
 
 
 
+; yap
+(require 'yasnippet)
+(yas-global-mode 1)
+
 
 (add-to-list 'auto-mode-alist '("\\.vue?\\'" . web-mode))
 
@@ -101,9 +105,86 @@
 (setq backup-directory-alist `(("." . "/tmp/emacs-backup")))
 (setq backup-by-copying t)
 (setq delete-old-versions t
-  kept-new-versions 6
-  kept-old-versions 2
-  version-control t)
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
 
+(when (eq system-type 'darwin)  ; mac specific bindings
+  (setq mac-right-command-modifier 'control ;
+
+        ))
+
+(defun xah-run-current-file ()
+  "Execute the current file.
+For example, if the current buffer is x.py, then it'll call 「python x.py」 in a shell. Output is printed to message buffer.
+The file can be Emacs Lisp, PHP, Perl, Python, Ruby, JavaScript, TypeScript, Bash, Ocaml, Visual Basic, TeX, Java, Clojure.
+File suffix is used to determine what program to run.
+
+If the file is modified or not saved, save it automatically before run.
+
+URL `http://ergoemacs.org/emacs/elisp_run_current_file.html'
+Version 2017-02-10"
+  (interactive)
+  (let (
+        (-suffix-map
+         ;; (‹extension› . ‹shell program name›)
+         `(
+           ("php" . "php")
+           ("pl" . "perl")
+           ("py" . "python")
+           ("py3" . ,(if (string-equal system-type "windows-nt") "c:/Python32/python.exe" "python3"))
+           ("rb" . "ruby")
+           ("go" . "go run")
+           ("js" . "node") ; node.js
+           ("ts" . "tsc --alwaysStrict --lib DOM,ES2015,DOM.Iterable,ScriptHost --target ES5") ; TypeScript
+           ("sh" . "bash")
+           ("clj" . "java -cp /home/xah/apps/clojure-1.6.0/clojure-1.6.0.jar clojure.main")
+           ("rkt" . "racket")
+           ("ml" . "ocaml")
+           ("vbs" . "cscript")
+           ("tex" . "pdflatex")
+           ("latex" . "pdflatex")
+           ("java" . "javac")
+           ;; ("pov" . "/usr/local/bin/povray +R2 +A0.1 +J1.2 +Am2 +Q9 +H480 +W640")
+           ))
+        -fname
+        -fSuffix
+        -prog-name
+        -cmd-str)
+    (when (not (buffer-file-name)) (save-buffer))
+    (when (buffer-modified-p) (save-buffer))
+    (setq -fname (buffer-file-name))
+    (setq -fSuffix (file-name-extension -fname))
+    (setq -prog-name (cdr (assoc -fSuffix -suffix-map)))
+    (setq -cmd-str (concat -prog-name " \""   -fname "\""))
+    (cond
+     ((string-equal -fSuffix "el") (load -fname))
+     ((string-equal -fSuffix "java")
+      (progn
+        (shell-command -cmd-str "*xah-run-current-file output*" )
+        (shell-command
+         (format "java %s" (file-name-sans-extension (file-name-nondirectory -fname))))))
+     (t (if -prog-name
+            (progn
+              (message "Running…")
+              (shell-command -cmd-str "*xah-run-current-file output*" ))
+          (message "No recognized program file suffix for this file."))))))
+
+(global-set-key (kbd "<f8>") 'xah-run-current-file)
+
+(setq org-enforce-todo-dependencies t)
+(setq org-agenda-include-diary t)
+(setq org-agenda-start-day "-1d")
+(setq org-agenda-span 30)
+(setq org-agenda-start-on-weekday nil)
+
+(setq org-directory "~/Dropbox/Apps/MobileOrg.bak2")
+;(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg.bak2")
+;(setq org-mobile-inbox-for-pull "~/Dropbox/Apps/MobileOrg.bak2/mobileorg.org")
+
+(setq org-default-notes-file (concat org-directory "/mobileorg.org"))
+(define-key global-map "\C-co" 'org-capture)
+
+(setq markdown-open-command "/Users/copyleft/config/bin/mark")
 
 (provide 'init-local)
